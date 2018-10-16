66934b11220d91df4bfe064cba23428e7d66730d
SonicGD <sonicgd@gmail.com>
2018-10-16 15:26:04 +0500

    Unify env names

--- src/BioEngine.Extra.IPB/IPBModule.cs
+++ src/BioEngine.Extra.IPB/IPBModule.cs
@@ -34,18 +34,18 @@ namespace BioEngine.Extra.IPB
         public override void ConfigureServices(WebHostBuilderContext context, IServiceCollection services)
         {
             base.ConfigureServices(context, services);
-            bool.TryParse(context.Configuration["IPB_API_DEV_MODE"] ?? "", out var devMode);
-            int.TryParse(context.Configuration["IPB_API_ADMIN_GROUP_ID"], out var adminGroupId);
-            int.TryParse(context.Configuration["IPB_API_PUBLISHER_GROUP_ID"], out var publisherGroupId);
-            int.TryParse(context.Configuration["IPB_API_EDITOR_GROUP_ID"], out var editorGroupId);
+            bool.TryParse(context.Configuration["BE_IPB_API_DEV_MODE"] ?? "", out var devMode);
+            int.TryParse(context.Configuration["BE_IPB_API_ADMIN_GROUP_ID"], out var adminGroupId);
+            int.TryParse(context.Configuration["BE_IPB_API_PUBLISHER_GROUP_ID"], out var publisherGroupId);
+            int.TryParse(context.Configuration["BE_IPB_API_EDITOR_GROUP_ID"], out var editorGroupId);
             services.Configure<IPBApiConfig>(config =>
             {
-                config.ApiUrl = new Uri(context.Configuration["IPB_API_URL"]);
+                config.ApiUrl = new Uri(context.Configuration["BE_IPB_API_URL"]);
                 config.DevMode = devMode;
                 config.AdminGroupId = adminGroupId;
                 config.PublisherGroupId = publisherGroupId;
                 config.EditorGroupId = editorGroupId;
-                config.ClientId = context.Configuration["IPB_API_CLIENT_ID"];
+                config.ClientId = context.Configuration["BE_IPB_API_CLIENT_ID"];
             });
             services.AddSingleton<IPBApiClientFactory>();
             services.AddMvc().AddApplicationPart(typeof(WebHostBuilderExtensions).Assembly);
