7518a51f61cfea99c7a589e8c1b2f41df39401aa
George <sonicgd@gmail.com>
2019-05-21 10:35:57 +0500

    Don't need startup filter anymore

--- src/BioEngine.Extra.IPB/IPBAuthModule.cs
+++ src/BioEngine.Extra.IPB/IPBAuthModule.cs
@@ -1,8 +1,5 @@
-using System;
 using BioEngine.Core.Modules;
 using BioEngine.Extra.IPB.Auth;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -11,24 +8,12 @@ namespace BioEngine.Extra.IPB
 {
     public class IPBAuthModule : BioEngineModule
     {
-        public override void ConfigureServices(IServiceCollection services, IConfiguration configuration, IHostEnvironment environment)
+        public override void ConfigureServices(IServiceCollection services, IConfiguration configuration,
+            IHostEnvironment environment)
         {
             services
                 .AddAuthentication("ipbToken")
                 .AddScheme<IPBTokenAuthOptions, TokenAuthenticationHandler>("ipbToken", null);
-            services.AddSingleton<IStartupFilter, IPBAuthStartupFilter>();
-        }
-    }
-
-    public class IPBAuthStartupFilter : IStartupFilter
-    {
-        public Action<IApplicationBuilder> Configure(Action<IApplicationBuilder> next)
-        {
-            return app =>
-            {
-                app.UseAuthentication();
-                next(app);
-            };
         }
     }
 }
